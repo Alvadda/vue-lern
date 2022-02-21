9b7b31d2ae53bde92729471e10000d8a5639136e
Alvadda <58438582+Alvadda@users.noreply.github.com>
2022-02-21 18:31:36 +0100

    vue start

---
+++ VueMastery/vue-real-word/src/views/EventDetails.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-if="event">
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
+
+<script>
+import { getEvent } from '@/services/eventService.js'
+
+export default {
+  props: ['id'],
+  data() {
+    return {
+      event: null,
+    }
+  },
+  async created() {
+    const response = await getEvent(this.id)
+    console.log(response.data)
+    this.event = response.data
+  },
+}
+</script>
---
+++ VueMastery/vue-real-word/src/views/EventList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="events">
+    <h1>Events For Good</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '@/components/EventCard.vue'
+import { getEvents } from '@/services/eventService.js'
+
+export default {
+  name: 'EventList',
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: null,
+    }
+  },
+
+  async created() {
+    const response = await getEvents()
+    this.events = response.data
+  },
+}
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
